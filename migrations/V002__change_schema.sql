cf88f27ce4aa2b782284fb9d7590ed8fe665127a
root <root@fhm4e9lig7qcmh1gacif.auto.internal>
2026-03-01 15:00:23 +0000

    fix normalization

--- migrations/V002__change_schema.sql
+++ migrations/V002__change_schema.sql
@@ -1,18 +1,20 @@
-ALTER TABLE product
-ADD COLUMN price double precision;
-
+ALTER TABLE product ADD COLUMN price double precision;
 UPDATE product p
 SET price = pi.price
 FROM product_info pi
 WHERE pi.product_id = p.id;
 
-ALTER TABLE orders
-ADD COLUMN date_created date;
-
+ALTER TABLE orders ADD COLUMN date_created date;
 UPDATE orders o
 SET date_created = od.date_created
 FROM orders_date od
 WHERE od.order_id = o.id;
+
+ALTER TABLE orders
+ADD CONSTRAINT orders_pkey PRIMARY KEY (id);
+
+ALTER TABLE product
+ADD CONSTRAINT product_pkey PRIMARY KEY (id);
 
 ALTER TABLE order_product
 ADD CONSTRAINT fk_order
